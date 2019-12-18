803615d829bc35e006ee4c047e83449f5f3e8e96
Administrator <admin@example.com>
2019-12-18 11:53:54 +0100

    ajout des classes Parametre et traitement du fichier JSON

---
+++ SimulationEconomique1/Parametre.cs
@@ -0,0 +1,11 @@
+﻿namespace SimulationEconomique1
+{
+    public class Parametre
+    {
+        public int Nombre_Individus { get; set; }
+        public double Repart { get; set; }
+        public int Mu { get; set; }
+        public int Sigma { get; set; }
+        public int Nb_Iteration { get; set; }
+    }
+}
--- SimulationEconomique1/Program.cs
+++ SimulationEconomique1/Program.cs
@@ -1,18 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace SimulationEconomique1
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            SimulationEconomique simulationEconomique = new SimulationEconomique(100, 0.5, 100, 20, 100);
-            simulationEconomique.process();
-            Console.ReadKey();
+            string jsonFile = "conf.json";
+            if (!File.Exists(jsonFile))
+            {
+                MessageBox.Show("Fichier json inexistant merci d'indiquer son emplacement");
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.InitialDirectory = Path.GetDirectoryName(Application.ExecutablePath) ;
+                openFileDialog1.Filter = "Fichier json (*.json)|*.json" ;
+                openFileDialog1.FilterIndex = 0;
+                openFileDialog1.RestoreDirectory = true ;
+
+                if(openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    jsonFile = openFileDialog1.FileName;
+                    //...
+                }
+                else
+                {
+                    MessageBox.Show("Erreur fichier");
+                    Application.Exit();
+                }
+            }
+            
+            using (StreamReader r = new StreamReader(jsonFile))
+            {
+                string json = r.ReadToEnd();
+                Parametre parametre = JsonConvert.DeserializeObject<Parametre>(json);
+                                    
+                SimulationEconomique simulationEconomique = new SimulationEconomique(parametre.Nombre_Individus, parametre.Repart, parametre.Mu, parametre.Sigma, parametre.Nb_Iteration);
+                simulationEconomique.process();
+                Console.ReadKey();
+            }
 
         }
 
